c9a0e584db08b805262ff29d0e252ed6d08c2911
rpathway <robbieyamashita1@byupathway.edu>
2026-05-10 20:35:38 -0400

    Updated getProfessionalData to return data using findOne method with id

--- week01/server/controller/professionalController.js
+++ week01/server/controller/professionalController.js
@@ -37,10 +37,9 @@ professionalFunctions.getProfessionalData = async function (req, res, next) {
   const client = new MongoClient(URI);
 
   try {
-    const data = await client.db('cse341').collection('week01-activity').find().toArray();
-    const choice = Math.floor(Math.random() * data.length);
+    const data = await client.db('cse341').collection('week01-activity').findOne({_id: '69fbac3a174b55485b26d77f'});
 
-    res.send(data[choice]);
+    res.send(data);
   } catch (e) {
     console.log(`Error in getProfessionalData: ${e}`)
     res.send({ status: `Error in getProfessionalData: ${e}` });
